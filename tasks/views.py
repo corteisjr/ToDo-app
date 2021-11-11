fd8f95f59e816a2ecd75942f9cfb21ad25a53c18
corteisjr <corteisjunior@gmail.com>
2021-11-11 11:18:56 +0200

    Other changes

--- tasks/views.py
+++ tasks/views.py
@@ -1,19 +1,28 @@
+from django.core import paginator
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from .models import Task
 from .forms import TaskForm
+from django.contrib import messages
 
 # Create your views here.
 
 def taskList(request):
 
-    tasks = Task.objects.all().order_by("-create_at")
+    tasks_list = Task.objects.all().order_by("-create_at")
+    paginator = Paginator(tasks_list, 3)
+    page = request.GET.get('page')
+    tasks = paginator.get_page(page)
+    
     return render(request, 'tasks/list.html', {'tasks': tasks})
 
+# read
 def taskView(request, id):
     task = get_object_or_404(Task, pk=id)
     return render(request, 'tasks/task.html', {'task': task})
 
+#Create
 def newTask(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
@@ -22,11 +31,38 @@ def newTask(request):
             task = form.save(commit=False)
             task.done = 'doing'
             task.save()
+            messages.info(request, 'Tarefa adicionada com sucesso')
             return redirect('/')
     else:
         form = TaskForm()
         return render(request, 'tasks/addtask.html', {'form': form})
 
+# update
+def editTask(request, id):
+    task = get_object_or_404(Task,  pk=id)
+    form = TaskForm(instance=task)
+
+    if(request.method == 'POST'):
+        form = TaskForm(request.POST, instance=task)
+
+        if form.is_valid():
+            task.save()
+            messages.info(request, 'Tarefa actualizada com sucesso')
+            return redirect('/')
+        else:
+             return render(request, 'tasks/edittask.html', {'form': form, 'task':task}) 
+    else:
+        return render(request, 'tasks/edittask.html', {'form': form, 'task':task})
+
+
+#delete
+
+def deleteTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    task.delete()
+
+    messages.info(request, 'Tarefa deletada com sucesso')
+    return redirect('/')
 
 def helloWorld(request):
     return HttpResponse('Hello World')
